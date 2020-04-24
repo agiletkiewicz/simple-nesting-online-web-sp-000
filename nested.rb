d878652580f0ab64aedf2c0e4078e2eb7fda2005
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-24 23:00:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,9 +64,6 @@ end
 
 def adding_matz
 
-
-
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -106,8 +103,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_key][:known_for] = "GUI"
      
+     programmer_hash
      
 end
 
