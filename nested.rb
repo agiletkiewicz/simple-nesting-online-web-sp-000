cab63df9c707d7bb172f6c893efd00e112a49cf7
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-24 22:57:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,7 @@ end
 
 def adding_matz
 
-programmer_hash = [:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 
 
 	programmer_hash = 
